2bee7ed91ded649461857e7cf1251724613f6173
StasTimoshanko <54182548+StasTimoshanko@users.noreply.github.com>
2019-08-31 12:42:13 +0300

    create master

---
+++ settings.py
@@ -0,0 +1,21 @@
+class Settings():
+
+    """Класс для хранения всех настроек игры Allien Invasion."""
+    def __init__(self):
+
+        """Инициализирует настройки игры"""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # настройка корабля
+        self.ship_speed_factor = 1.5
+        # Параметры пули
+        self.bullet_speed_factor = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3 # ограничение пуль
+
+
+
