5681623b2cf2fc2ef8879fb759c917544017bbee
StasTimoshanko <54182548+StasTimoshanko@users.noreply.github.com>
2019-09-05 00:34:15 +0300

    add button "Play"

--- game_functions.py
+++ game_functions.py
@@ -31,7 +31,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
 
     """обрабатывает нажатие клавиш и события мышм"""
 
@@ -42,9 +42,26 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen, stats,  play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(ai_settings, screen, stats,  play_button, ship, aliens, bullets, mouse_x, mouse_y):
+
+    """Запускает новую игру при нажатии кнопки Play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.reset_stats()
+        stats.game_active = True
+        #Очистка списков пришельцев и пуль
+        aliens.empty()
+        bullets.empty()
+        #Создание нового флота и рамещение корабля в центре
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+
+
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     # При каждом проходе цикла перерисовывается экран
     screen.fill(ai_settings.bg_color)
     # Все пули выводятся позади изоражений коробля и пришельцев.
@@ -52,6 +69,9 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+    #кнопка Play отображается в том случае, если игра неактивна
+    if not stats.game_active:
+        play_button.draw_button()
     # отображение последнего прорисованного экрана.
     pygame.display.flip()
 
